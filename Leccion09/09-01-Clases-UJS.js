afc4d33c214b141aaf5cd1b8a53deb8de9794883
jared255 <jaredcamargo255@gmail.com>
2024-09-09 23:47:43 -0400

    Métodos get y set en JavaScript

--- Leccion09/09-01-Clases-UJS.js
+++ Leccion09/09-01-Clases-UJS.js
@@ -1,7 +1,13 @@
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        this._nombre = nombre;
     }
 }
 // el constructor no se manda a llamar
@@ -12,3 +18,12 @@ console.log(persona1);
 
 let persona2 = new Persona ('Rene', 'Descartes');
 console.log(persona2);
+
+// Métodos get y set en JavaScript
+// se agregó get a la clase Persona para acceder a la propiedad nombre
+
+console.log(persona1.nombre);
+
+// se agregó set a la clase Persona para modificar la propiedad nombre
+persona1.nombre = 'Jorge';//set nombre('Jorge')
+console.log(persona1.nombre);//get nombre
